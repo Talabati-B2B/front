99d11e472ebefae940fe36749b5fac469829e59d
AseelFannana <as421486614@gmail.com>
2026-07-07 12:05:39 +0200

    save changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-import Register from "./pages/auth/register/Register";
-import Login from "./pages/auth/login/Login";
-import ForgotPassword from "./pages/auth/forgot-password/ForgotPassword";
-import ResetPassword from "./pages/auth/reset-password/ResetPassword";
 import LandingPage from "./pages/landing-page/LandingPage";
 
 export default function App() {
@@ -10,10 +6,6 @@ export default function App() {
     <Routes>
       <Route path="/" element={<LandingPage />} />
 
-      <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/forgot-password" element={<ForgotPassword />} />
-      <Route path="/reset-password" element={<ResetPassword />} />
     </Routes>
   );
 }
