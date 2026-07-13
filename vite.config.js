2056208b09391b0adfb2bfe7b5f329e489100730
Dana-Nabeel <danans9710@gmail.com>
2026-07-13 11:03:42 +0300

    resolve merge conflicts

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-<<<<<<< HEAD
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react() , tailwindcss()],
-=======
-import tailwindcss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
->>>>>>> 95d56ba12a607e167a3fbc93c010210ab0da3b42
 })
